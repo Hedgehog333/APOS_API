76ed060d56816868e6ea0d4e5cacd6f6846c3de2
Hedgehog333 <artem1100.94@gmail.com>
2018-05-31 17:06:51 +0300

    fix conection

--- app/dao/Connect.scala
+++ app/dao/Connect.scala
@@ -3,7 +3,7 @@ package dao
 import play.api.db.{Database, Databases}
 
 trait Connect[T] {
-  def withDatabase(block: Database => T): Unit = {
+  def withDatabase[T](block: Database => T): Unit = {
     Databases.withDatabase(
       driver = "com.mysql.jdbc.Driver",
       url = "jdbc:mysql://localhost:3306/APOS",
