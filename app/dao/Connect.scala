3bdbfd57deb0ca736bd65e2328e5327a25b76953
Hedgehog333 <artem1100.94@gmail.com>
2018-06-01 05:28:02 +0300

    edit url

--- app/dao/Connect.scala
+++ app/dao/Connect.scala
@@ -6,7 +6,7 @@ trait Connect[T] {
   def withDatabase[T](block: Database => T): Unit = {
     Databases.withDatabase(
       driver = "com.mysql.jdbc.Driver",
-      url = "jdbc:mysql://localhost:3306/APOS",
+      url = "jdbc:mysql://127.0.0.1:3306/APOS",
 
       //TODO: User database credentials
       config = Map(
